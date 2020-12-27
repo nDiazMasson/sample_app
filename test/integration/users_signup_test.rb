7db1447c38df62206957a1c716af2e8c45cfc2d4
Nico <ndiazmasson@gmail.com>
2020-12-27 00:40:51 -0700

    SSL & PUMA

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -20,12 +20,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "valid signup information" do
     get signup_path
+    
     assert_difference "User.count", 1 do
-      post users_path, params{
+      post users_path, params:{
         user:{
           name: "Example User",
-          email: "user@example.com"
-          password: "password"
+          email: "user@example.com",
+          password: "password",
           password_confirmation: "password"
         }
       }
@@ -34,6 +35,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
   end
-  end
+
 
 end
